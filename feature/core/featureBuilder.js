6031ef56ff3a2aa2e53b6806d3c3847436b75721
rlepennec <rlepennec@yahoo.fr>
2023-03-02 09:45:43 +0100

    drop focus

--- feature/core/featureBuilder.js
+++ feature/core/featureBuilder.js
@@ -50,11 +50,12 @@ export class FeatureBuilder {
     }
 
     /**
-     * @param item The embedded item from which to create the feature.
+     * @param type The type of feature to create.
+     * @param item The item of the feature.
      * @returns the new feature. 
      */
-    createFromEmbedded(item) {
-        switch (item?.type) {
+    createFeature(type, item) {
+        switch (type) {
             case 'appel':
                 return new Appel(this.actor, item, this.periode);
             case 'atlanteide':
@@ -73,6 +74,8 @@ export class FeatureBuilder {
                 return new Rite(this.actor, item, this.periode);
             case 'rituel':
                 return new Rituel(this.actor, item, this.periode);
+            case 'science':
+                return new Science(this.actor, item, this.periode);
             case 'sort':
                 return new Sort(this.actor, item, this.periode);
             case 'technique':
@@ -85,34 +88,51 @@ export class FeatureBuilder {
     }
 
     /**
+     * @param item The embedded item from which to create the feature.
+     * @returns the new feature. 
+     */
+    createFromEmbedded(item) {
+        switch (item?.type) {
+
+            case 'appel':
+            case 'atlanteide':
+            case 'dracomachie':
+            case 'formule':
+            case 'habitus':
+            case 'invocation':
+            case 'pratique':
+            case 'rite':
+            case 'rituel':
+            case 'sort':
+            case 'technique':
+            case 'tekhne':
+                return this.createFeature(item?.type, item);
+
+            default:
+                return null;
+        }
+    }
+
+    /**
      * @param item The original focus item object.
      */
     async dropOriginal(item) {
         switch (item?.type) {
+
             case 'appel':
-                return Appel(this.actor, item, this.periode).dropOriginal(item);
             case 'atlanteide':
-                return new Atlanteide(this.actor, item, this.periode).dropOriginal(item);
             case 'dracomachie':
-                return new Dracomachie(this.actor, item, this.periode).dropOriginal(item);
             case 'formule':
-                return new Formule(this.actor, null, this.periode).dropOriginal(item);
             case 'habitus':
-                return new Habitus(this.actor, item, this.periode).dropOriginal(item);
             case 'invocation':
-                return new Invocation(this.actor, item, this.periode).dropOriginal(item);
             case 'pratique':
-                return new Pratique(this.actor, item, this.periode).dropOriginal(item);
             case 'rite':
-                return new Rite(this.actor, item, this.periode).dropOriginal(item);
             case 'rituel':
-                return new Rituel(this.actor, item, this.periode).dropOriginal(item);
             case 'sort':
-                return new Sort(this.actor, item, this.periode).dropOriginal(item);
             case 'technique':
-                return new Technique(this.actor, item, this.periode).dropOriginal(item);
             case 'tekhne':
-                return new Tekhne(this.actor, item, this.periode).dropOriginal(item);
+                return this.createFeature(item?.type, null).dropOriginal(item);
+
             default:
                 return null;
         }
