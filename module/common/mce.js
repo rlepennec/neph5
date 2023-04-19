a82a00364fe309e415222aee744e881f0932d074
rlepennec <rlepennec@yahoo.fr>
2023-04-19 12:45:21 +0200

    css

---
+++ module/common/mce.js
@@ -0,0 +1,12 @@
+export class MCE {
+
+    static CONFIG = {
+        branding: false,
+        menubar: false,
+        statusbar: false,
+        content_css: ["systems/neph5e/module/common/mce.css"],
+        plugins: "lists image table code save link",
+        toolbar: "styles bullist numlist image table hr link removeformat code save",
+        save_enablewhendirty: true
+    };
+};
