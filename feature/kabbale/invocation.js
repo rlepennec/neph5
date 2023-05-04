456ac45ca4e82216e279acf874ef216009061026
rlepennec <rlepennec@yahoo.fr>
2023-05-04 13:52:13 +0200

    fix roll

--- feature/kabbale/invocation.js
+++ feature/kabbale/invocation.js
@@ -11,7 +11,7 @@ export class Invocation extends AbstractFocus {
      */
     async initializeRoll() {
 
-        if (this.embedded.system.focus !== true && this.embedded.system.status === 'dechiffre') {
+        if (this.embedded == null || this.embedded.system.focus !== true && this.embedded.system.status === 'dechiffre') {
             ui.notifications.warn("Vous ne possédez pas le focus de cette invocation");
             return;
         }
