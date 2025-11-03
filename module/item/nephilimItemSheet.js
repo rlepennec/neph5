e50ef54b9c6eccb89647715eeb69e4ff4a5fcc90
Kartaj <rlepennec@yahoo.fr>
2025-11-03 19:55:00 +0100

    cleanup

--- module/item/nephilimItemSheet.js
+++ module/item/nephilimItemSheet.js
@@ -42,110 +42,110 @@ export class NephilimItemSheet extends HandlebarsApplicationMixin(ItemSheetV2) {
         return context
     }
 
-  /**
-   * Create drag-and-drop workflow handlers for this Application
-   * @returns {DragDrop[]}     An array of DragDrop handlers
-   * @private
-   */
-  #createDragDropHandlers() {
-    return this.options.dragDrop.map((d) => {
-      d.permissions = {
-        dragstart: this._canDragStart.bind(this),
-        drop: this._canDragDrop.bind(this),
-      };
-      d.callbacks = {
-        dragstart: this._onDragStart.bind(this),
-        dragover: this._onDragOver.bind(this),
-        drop: this._onDrop.bind(this),
-      };
-      return new foundry.applications.ux.DragDrop.implementation(d);
-    });
-  }
-
-  #dragDrop;
-
-  // Optional: Add getter to access the private property
-
-  /**
-   * Returns an array of DragDrop instances
-   * @type {DragDrop[]}
-   */
-  get dragDrop() {
-    return this.#dragDrop;
-  }
-
-  /**
-   * Actions performed after any render of the Application.
-   * Post-render steps are not awaited by the render process.
-   * @param {ApplicationRenderContext} context      Prepared context data
-   * @param {RenderOptions} options                 Provided render options
-   * @protected
-   */
-  _onRender(context, options) {
-    this.#dragDrop.forEach((d) => d.bind(this.element));
-  }
-
-  /**
-   * Define whether a user is able to begin a dragstart workflow for a given drag selector
-   * @param {string} selector       The candidate HTML selector for dragging
-   * @returns {boolean}             Can the current user drag this selector?
-   * @protected
-   */
-  _canDragStart(selector) {
-    // game.user fetches the current user
-    return this.isEditable;
-  }
-
-  /**
-   * Define whether a user is able to conclude a drag-and-drop workflow for a given drop selector
-   * @param {string} selector       The candidate HTML selector for the drop target
-   * @returns {boolean}             Can the current user drop on this selector?
-   * @protected
-   */
-  _canDragDrop(selector) {
-    // game.user fetches the current user
-    return this.isEditable;
-  }
-
-  /**
-   * Callback actions which occur at the beginning of a drag start workflow.
-   * @param {DragEvent} event       The originating DragEvent
-   * @protected
-   */
-  _onDragStart(event) {
-    const el = event.currentTarget;
-    if ('link' in event.target.dataset) return;
-
-    // Extract the data you need
-    let dragData = null;
-
-    if (!dragData) return;
-
-    // Set data transfer
-    event.dataTransfer.setData('text/plain', JSON.stringify(dragData));
-  }
-
-  /**
-   * Callback actions which occur when a dragged element is over a drop target.
-   * @param {DragEvent} event       The originating DragEvent
-   * @protected
-   */
-  _onDragOver(event) {}
-
-
-  /**
-   * Callback actions which occur when a dragged element is dropped on a target.
-   * @param {DragEvent} event       The originating DragEvent
-   * @protected
-   */
-  async _onDrop(event) {
-    console.log("onDrop");
-    const data = TextEditor.getDragEventData(event);
-
-    // Handle different data types
-    switch (data.type) {
-        // write your cases
+    /**
+     * Create drag-and-drop workflow handlers for this Application
+     * @returns {DragDrop[]}     An array of DragDrop handlers
+     * @private
+     */
+    #createDragDropHandlers() {
+        return this.options.dragDrop.map((d) => {
+        d.permissions = {
+            dragstart: this._canDragStart.bind(this),
+            drop: this._canDragDrop.bind(this),
+        };
+        d.callbacks = {
+            dragstart: this._onDragStart.bind(this),
+            dragover: this._onDragOver.bind(this),
+            drop: this._onDrop.bind(this),
+        };
+        return new foundry.applications.ux.DragDrop.implementation(d);
+        });
     }
-  }
+
+    #dragDrop;
+
+    // Optional: Add getter to access the private property
+
+    /**
+     * Returns an array of DragDrop instances
+     * @type {DragDrop[]}
+     */
+    get dragDrop() {
+        return this.#dragDrop;
+    }
+
+    /**
+     * Actions performed after any render of the Application.
+     * Post-render steps are not awaited by the render process.
+     * @param {ApplicationRenderContext} context      Prepared context data
+     * @param {RenderOptions} options                 Provided render options
+     * @protected
+     */
+    _onRender(context, options) {
+        this.#dragDrop.forEach((d) => d.bind(this.element));
+    }
+
+    /**
+     * Define whether a user is able to begin a dragstart workflow for a given drag selector
+     * @param {string} selector       The candidate HTML selector for dragging
+     * @returns {boolean}             Can the current user drag this selector?
+     * @protected
+     */
+    _canDragStart(selector) {
+        // game.user fetches the current user
+        return this.isEditable;
+    }
+
+    /**
+     * Define whether a user is able to conclude a drag-and-drop workflow for a given drop selector
+     * @param {string} selector       The candidate HTML selector for the drop target
+     * @returns {boolean}             Can the current user drop on this selector?
+     * @protected
+     */
+    _canDragDrop(selector) {
+        // game.user fetches the current user
+        return this.isEditable;
+    }
+
+    /**
+     * Callback actions which occur at the beginning of a drag start workflow.
+     * @param {DragEvent} event       The originating DragEvent
+     * @protected
+     */
+    _onDragStart(event) {
+        const el = event.currentTarget;
+        if ('link' in event.target.dataset) return;
+
+        // Extract the data you need
+        let dragData = null;
+
+        if (!dragData) return;
+
+        // Set data transfer
+        event.dataTransfer.setData('text/plain', JSON.stringify(dragData));
+    }
+
+    /**
+     * Callback actions which occur when a dragged element is over a drop target.
+     * @param {DragEvent} event       The originating DragEvent
+     * @protected
+     */
+    _onDragOver(event) {}
+
+
+    /**
+     * Callback actions which occur when a dragged element is dropped on a target.
+     * @param {DragEvent} event       The originating DragEvent
+     * @protected
+     */
+    async _onDrop(event) {
+        console.log("onDrop");
+        const data = TextEditor.getDragEventData(event);
+
+        // Handle different data types
+        switch (data.type) {
+            // write your cases
+        }
+    }
 
 }
