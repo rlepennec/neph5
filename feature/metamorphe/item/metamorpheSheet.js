852c4a41b6f499227bc3674b53afaadd51ee884d
Kartaj <rlepennec@yahoo.fr>
2025-12-20 11:09:15 +0100

    metamorphe

--- feature/metamorphe/item/metamorpheSheet.js
+++ feature/metamorphe/item/metamorpheSheet.js
@@ -8,6 +8,9 @@ export class MetamorpheSheet extends NephilimItemSheet {
     static DEFAULT_OPTIONS = {
         id: this.#ID,
         classes: [this.#ID],
+        form: {
+            handler: MetamorpheSheet.#onSubmit,
+        },
         position: {
             height: 500,
             width: 590,
@@ -33,22 +36,62 @@ export class MetamorpheSheet extends NephilimItemSheet {
         }
     }
 
+    static #onSubmit(event, form, formData) {
+
+        console.log(formData);
+
+        /*
+        const settings = foundry.utils.expandObject(formData.object);
+        await Promise.all(
+            Object.entries(settings).map(([key, value]) => game.settings.set("foo", key, value))
+        );
+        */
+    }
+
+
     /**
      * @override
      */
     _updateObject(event, formData) {
 
-        // Update metamorphoses
-        const v5 = [];
-        for (let index = 0; index < 10; index++) {
-            const name = "system.metamorphoses.v5.[" + index + "]";
-            v5.push({ name: formData[name + ".name"] });
-            delete formData[name + ".name"];
-        }
-        formData["system.metamorphoses.v5"] = v5;
+
+        console.log(formData);
 
         // Update object
         super._updateObject(event, formData);
     }
+
+
+
+    /**
+     * @override
+     */
+    /*
+    _updateObject(event, formData) {
+
+        // Update voies
+        if (formData["system.cercle"] === "basseMagie") {
+            formData["system.voies"] = [];
+        } else {
+            let size = this.item.system.voies == null ? 0 : this.item.system.voies.length;
+            const voies = [];
+            for (let index = 0; index < size; index++) {
+                const name = "system.voies.[" + index + "]";
+                voies.push(formData[name]);
+                delete formData[name];
+            }
+            formData["system.voies"] = voies;
+        }
+
+        // Update syntaxe & incantation
+        if (formData["system.cercle"] !== "grandSecret") {
+            formData['system.-=syntaxe'] = null;
+            formData['system.-=incantation'] = null;
+        }
+
+        // Update object
+        super._updateObject(event, formData);
+    }
+        */
 
 }
