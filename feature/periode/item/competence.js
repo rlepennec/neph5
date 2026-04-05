1fd44681c3a0008cced74f96e4908af1f26a1db2
Kartaj <rlepennec@yahoo.fr>
2026-04-05 15:35:18 +0200

    clean prepare context

--- feature/periode/item/competence.js
+++ feature/periode/item/competence.js
@@ -29,13 +29,4 @@ export class CompetenceSheet extends NephilimItemSheet {
         }
     }
 
-    /** 
-     * @override
-     */
-    getOriginalData() {
-        return {
-            elements: Game.pentacle.elements
-        }
-    }
-
 }
