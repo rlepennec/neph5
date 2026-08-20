ef99a69c5459c90b2971a0acb2d5028ca3448e84
rlepennec <rlepennec@yahoo.fr>
2026-08-20 23:03:07 +0200

    ashbury atlanteide

--- feature/atlanteide/item/atlanteide.js
+++ feature/atlanteide/item/atlanteide.js
@@ -5,8 +5,8 @@ export class AtlanteideSheet extends NephilimItemSheet {
     static DEFAULT_OPTIONS = {
         classes: ["vk-atlanteide"],
         position: {
-            width: 950,
-            height: 700
+            width: 1220,
+            height: 720
         }
     }
 
--- feature/atlanteide/item/atlanteide.mjs
+++ feature/atlanteide/item/atlanteide.mjs
@@ -21,6 +21,7 @@ export class AtlanteideDataModel extends foundry.abstract.TypeDataModel {
             ),
             degre: new foundry.data.fields.NumberField(
                 {
+                    initial: 1,
                     required: false
                 }
             ),
@@ -33,7 +34,7 @@ export class AtlanteideDataModel extends foundry.abstract.TypeDataModel {
             (
                 {
                     categories: ["IMAGE"],
-                    initial: "systems/neph5e/assets/vk/items/atlanteides-defaut.webp"
+                    initial: "systems/neph5e/assets/vk/items/atlanteide-defaut.png"
                 }
             ),
         }
